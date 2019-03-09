fff0a4891742e0e7559ae2da82032d7d695982e1
Naresh <naresh.kumar@thoughtspot.com>
2019-03-08 22:07:29 -0800

    Add generic setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,22 @@
 cd ~/
+if [ "$(uname)" == "Darwin" ]; then
+  echo "Setting up Mac"
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  brew install git
+  brew install vim
+  brew install cmake
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+  if [ "$(cat /etc/os-release | head -1 | cut -b 7-12)" == "Ubuntu" ]; then
+    echo "Setting up Ubuntu"
+    sudo apt-get install git vim cmake
+  elif [ "$(cat /etc/os-release | head -1 | cut -b 7-12)" == "CentOS" ]; then
+    echo "Setting up CentOS"
+    sudo yum install git vim cmake
+  fi
+fi
+
+git clone https://github.com/nemausus/dotfiles.git
+
 # setup all symlinks
 ln -sf dotfiles/bashrc .bashrc
 ln -sf dotfiles/vimrc .vimrc
